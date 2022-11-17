f4c8c7a998ee4205df584aed4204f671754ae383
exploding60 <rizkysyahputra14045@gmail.com>
2022-11-17 23:45:30 +0700

    fixing cloudinary 2

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,9 +2,6 @@ const multer = require("multer");
 const { response } = require("./../middleware/getDataHelpers");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./tmp");
-  },
   filename: function (req, file, cb) {
     const uniq = Date.now() + Math.round(Math.random() * 1e9);
     cb(null, file.fieldname + "-" + uniq + ".png");
